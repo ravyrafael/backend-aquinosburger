a7c7efbd088876ab4b490ffcdbc815f676d4743e
Ravy Freitas <ravy.freitas@meta3.com.br>
2019-05-27 20:58:18 -0300

    changes on CORS

--- server.js
+++ server.js
@@ -9,14 +9,14 @@ const cors = require('cors');
 //Iniciando app
 const app = express();
 app.use(express.json());
-app.use(cors);
+app.use(cors());
 
 //Iniciando Banco de dados
 mongoose.connect(
-    `${process.env.MONGO_URL}`
+    process.env.MONGO_URL
 ,{ 
     useNewUrlParser: true,
-    dbName:`${process.env.DATABASE}`
+    dbName:process.env.DATABASE
  });
 
 //Models
@@ -24,7 +24,6 @@ requireDir('./src/models');
 
 
 app.use('/api', require('./src/routes'));
-
 
 
 app.listen(process.env.PORT);
