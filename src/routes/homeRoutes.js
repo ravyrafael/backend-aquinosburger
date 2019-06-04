10b35957233973636a2d3ed40d7eb139928f8c57
Ravy Freitas <ravy.freitas@meta3.com.br>
2019-06-04 13:30:55 -0300

    definidas as rotas de erro

--- src/routes/homeRoutes.js
+++ src/routes/homeRoutes.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const routes = express.Router();
 
-const ObjectController = require('../controllers/ObjectController');
 const HomeController = require('../controllers/HomeController');
 //Rotas
 
